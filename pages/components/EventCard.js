96a413633dbac1789fa9851313a3e03c26d8b1da
Dan-Sutton <dewasutton@gmail.com>
2022-12-06 19:24:09 +0000

    rendering posts

--- pages/components/EventCard.js
+++ pages/components/EventCard.js
@@ -1,12 +1,29 @@
 import React from "react";
 import Link from "next/link";
+import styles from "../../styles/EventCard.module.css";
+import Image from "next/image";
 
-function eventCard({ date, id, slug, title, updatedAt }) {
+function EventCard({
+  date,
+  id,
+  slug,
+  title,
+  updatedAt,
+  coverPhoto,
+  description,
+}) {
   return (
     <div>
-      <Link></Link>
+      <Link href={"/events/" + slug}>
+        <h1>{title}</h1>
+        <p>{date}</p>
+        <div className={styles.imgContainer}>
+          <Image width={400} height={200} src={coverPhoto.url} alt=""></Image>
+        </div>
+        <p>{description}</p>
+      </Link>
     </div>
   );
 }
 
-export default eventCard;
+export default EventCard;
