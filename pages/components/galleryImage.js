1445ae75e52fed5c8d31d874856f4ec1dadb1633
Dan-Sutton <dewasutton@gmail.com>
2022-12-08 14:40:11 +0000

    desc hover

--- pages/components/galleryImage.js
+++ pages/components/galleryImage.js
@@ -1,12 +1,37 @@
 import React from "react";
 import Image from "next/image";
+import { useState } from "react";
+import styles from "../../styles/galleryImage.module.css";
 
-function GalleryImage({ images }) {
+function GalleryImage({ image, desc, index, setViewDesc, viewDesc }) {
   return (
     <div>
-      {images.map((image) => {
-        return <Image height={200} width={200} alt="" src={image.src} />;
-      })}
+      <div className={styles.imageContainer}>
+        {viewDesc[0] && index === viewDesc[1] ? (
+          <p
+            index={index}
+            className={styles.desc}
+            onMouseLeave={() => {
+              setViewDesc([false, null]);
+            }}
+          >
+            {desc}
+          </p>
+        ) : (
+          <></>
+        )}
+
+        <Image
+          onMouseEnter={() => {
+            setViewDesc([true, index]);
+          }}
+          className={styles.galleryImage}
+          height={200}
+          width={200}
+          alt=""
+          src={image}
+        />
+      </div>
     </div>
   );
 }
--- pages/gallery.js
+++ pages/gallery.js
@@ -3,8 +3,45 @@ import NavButton from "./components/NavButton";
 import styles from "../styles/gallery.module.css";
 import { motion } from "framer-motion";
 import GalleryImage from "./components/galleryImage";
+import { useState } from "react";
 
 function gallery(props) {
+  const [viewDesc, setViewDesc] = useState([false, null]);
+  const images = [
+    {
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUFKYcXuM4onp47mQIIb5AgwVG6EXUWuZ6HGw7-7s&s",
+      desc: "Doggo pulling a weird one",
+    },
+    {
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUFKYcXuM4onp47mQIIb5AgwVG6EXUWuZ6HGw7-7s&s",
+      desc: "Doggo pulling a weird one",
+    },
+    {
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUFKYcXuM4onp47mQIIb5AgwVG6EXUWuZ6HGw7-7s&s",
+      desc: "Doggo pulling a weird one",
+    },
+    {
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUFKYcXuM4onp47mQIIb5AgwVG6EXUWuZ6HGw7-7s&s",
+      desc: "Doggo pulling a weird one",
+    },
+    {
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUFKYcXuM4onp47mQIIb5AgwVG6EXUWuZ6HGw7-7s&s",
+      desc: "Doggo pulling a weird one",
+    },
+    {
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUFKYcXuM4onp47mQIIb5AgwVG6EXUWuZ6HGw7-7s&s",
+      desc: "Doggo pulling a weird one",
+    },
+    {
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUFKYcXuM4onp47mQIIb5AgwVG6EXUWuZ6HGw7-7s&s",
+      desc: "Doggo pulling a weird one",
+    },
+    {
+      src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUFKYcXuM4onp47mQIIb5AgwVG6EXUWuZ6HGw7-7s&s",
+      desc: "Doggo pulling a weird one",
+    },
+  ];
+
   return (
     <div className={styles.gallery}>
       <div className={styles.navposition}>
@@ -14,26 +51,19 @@ function gallery(props) {
       <div className={styles.galleryContainer}>
         <h1>GALLERY</h1>
 
-        <GalleryImage
-          images={[
-            {
-              src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUFKYcXuM4onp47mQIIb5AgwVG6EXUWuZ6HGw7-7s&s",
-              desc: "Doggo pulling a weird one",
-            },
-            {
-              src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUFKYcXuM4onp47mQIIb5AgwVG6EXUWuZ6HGw7-7s&s",
-              desc: "Doggo pulling a weird one",
-            },
-            {
-              src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUFKYcXuM4onp47mQIIb5AgwVG6EXUWuZ6HGw7-7s&s",
-              desc: "Doggo pulling a weird one",
-            },
-            {
-              src: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSUFKYcXuM4onp47mQIIb5AgwVG6EXUWuZ6HGw7-7s&s",
-              desc: "Doggo pulling a weird one",
-            },
-          ]}
-        />
+        <div className={styles.images}>
+          {images.map((image, index) => {
+            return (
+              <GalleryImage
+                viewDesc={viewDesc}
+                setViewDesc={setViewDesc}
+                image={image.src}
+                desc={image.desc}
+                index={index}
+              />
+            );
+          })}
+        </div>
       </div>
     </div>
   );
