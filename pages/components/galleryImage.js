c2d2912fe3c4a39ec0a883120207d5c36a7ddb97
Dan-Sutton <dewasutton@gmail.com>
2022-12-08 14:56:53 +0000

    image modal

--- pages/components/galleryImage.js
+++ pages/components/galleryImage.js
@@ -3,13 +3,15 @@ import Image from "next/image";
 import { useState } from "react";
 import styles from "../../styles/galleryImage.module.css";
 import { motion } from "framer-motion";
+import Swal from "sweetalert2";
 
 function GalleryImage({ image, desc, index, setViewDesc, viewDesc }) {
   return (
     <div>
       <div className={styles.imageContainer}>
         {viewDesc[0] && index === viewDesc[1] ? (
-          <motion.p
+          <motion.div
+            className={styles.desc}
             initial={{ opacity: 0 }}
             animate={{
               opacity: 1,
@@ -19,14 +21,30 @@ function GalleryImage({ image, desc, index, setViewDesc, viewDesc }) {
                 bounce: 0.2,
               },
             }}
-            index={index}
-            className={styles.desc}
-            onMouseLeave={() => {
-              setViewDesc([false, null]);
-            }}
           >
-            {desc}
-          </motion.p>
+            <p
+              index={index}
+              onMouseLeave={() => {
+                setViewDesc([false, null]);
+              }}
+            >
+              {desc}
+            </p>
+            <button
+              onClick={() => {
+                Swal.fire({
+                  showConfirmButton: false,
+                  imageUrl: image,
+                  imageWidth: "100%",
+                  imageHeight: "100%",
+                  imageAlt: "Custom image",
+                  background: "rgba(24, 24, 24, 0)",
+                });
+              }}
+            >
+              VIEW
+            </button>
+          </motion.div>
         ) : (
           <></>
         )}
